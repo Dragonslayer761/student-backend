83bfbadc7a0548c32f9310cc996a7a649ea9edfd
subham chowdhury <sc761851@gmail.com>
2021-07-20 21:44:55 +0530

    course add,get and student add get route added

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,11 @@ var course = require('../public/json/course');
 var admin = require('../public/json/admin');
 var jwt = require('jsonwebtoken');
 var knex = require('knex');
+var bcrypt = require('bcrypt')
+
+//salt rounds
+
+const saltRounds = 10
 
 //db connection
 const db = knex({
@@ -17,10 +22,7 @@ const db = knex({
   }
 });
 
-//db test
-db.select('*').from('login').then(data =>{
-  console.log(data[0])
-})
+
 
 //secret key
 const key = "sEcReTKeY"
@@ -36,6 +38,7 @@ const getToken = (req, res, next) => {
   }
 
 }
+
 //login and access token
 router.post('/login', (req, res) => {
   const { username, password } = req.body
@@ -64,24 +67,67 @@ router.post('/login', (req, res) => {
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-  db.select('*').from('course').then(data =>{
+  db.select('*').from('course').then(data => {
     res.send(data)
-    })
+  })
 });
-//register
-router.post('/register',(req, res)=>{
-  const{username,fname,lname,email,phoneno,dob,country,joining_year,password} = req.body;
-  
+
+//register user
+router.post('/register/admin', (req, res) => {
+  const { a_id, full_name, email, password } = req.body;
+  console.log(a_id)
+  const salt = bcrypt.genSaltSync(saltRounds);
+  let hash = bcrypt.hashSync(password, salt)
+  db.transaction(trx => {
+    trx.insert({
+      a_id: a_id,
+      full_name: full_name,
+      email: email,
+      password_hash: hash
+    }).into('admin_user').returning('email')
+      .then(email => {
+        res.json(email[0]);
+      }).then(trx.commit)
+      .catch(trx.rollback)
+  }).catch(err => res.status(404).json('unable to register'))
 })
+
+
 //get all student
 router.get('/student', getToken, (req, res) => {
-  res.send(JSON.stringify(student));
+  db.select('first_name','last_name').from('student').then(name =>{
+    res.send(name)
+  }).catch(err => {
+    res.sendStatus(404).send(err)
+  })
 })
 
-
+//register new student
+router.post('/register/student',(req,res)=>{
+  const{s_id,first_name,last_name,email,phone_number,date_of_birth,country,joining_date} = req.body;
+  console.log(s_id)
+  db.transaction(trx =>{
+    trx.insert({
+        s_id:s_id,
+        first_name:first_name,
+        last_name:last_name,
+        email:email,
+        phone_number:phone_number,
+        date_of_birth:date_of_birth,
+        country:country,
+        joining_date:joining_date
+    }).into('student').returning('first_name')
+    .then(first_name=>{
+      res.send(first_name[0])
+    }).then(trx.commit)
+    .catch(trx.rollback)
+  }).catch(err => res.status(404).json(err.detail))
+})
 
 // get student by its id
 router.get('/student/:id', getToken, (req, res) => {
+  const { username } = req.body;
+  console.log(username)
   jwt.verify(req.token, key, (err, authData) => {
     if (err) {
       res.sendStatus(403)
@@ -94,7 +140,7 @@ router.get('/student/:id', getToken, (req, res) => {
         }
       })
       if (user) {
-        res.send({user,authData})
+        res.send({ user, authData, username })
       } else {
         res.sendStatus(404)
       }
@@ -117,6 +163,26 @@ router.get('/courses', getToken, (req, res) => {
   res.send(course)
 });
 
+
+//add courses
+router.post('/register/course',(req,res)=>{
+  const {c_id,course_name,course_duration,t_credit,p_credit,j_credit}=req.body
+  db.transaction(trx=>{
+    trx.insert({
+      c_id:c_id,
+      course_name:course_name,
+      course_duration:course_duration,
+      t_credit:t_credit,
+      p_credit:p_credit,
+      j_credit:j_credit
+    }).into('course').returning('course_name')
+    .then(name =>{
+      res.send(name)
+    })
+    .then(trx.commit)
+    .catch(trx.rollback)
+  }).catch(err => res.status(404).json(err.detail))
+})
 
 //export the route
 module.exports = router;
